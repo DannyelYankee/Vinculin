892684559a570e9eba948aed3178fd5416cef698
DannyelYankee <daniel.juape@gmail.com>
2021-10-04 20:55:02 +0200

    Vídeo 5

--- app/index.php
+++ app/index.php
@@ -1,35 +1,105 @@
 <!DOCTYPE html>
 <html lang="es">
-  <meta charset="iso-8559-1"/>
-  <meta name="description" content="Simulación de Linkedin"/>
-  <title> Vinculín </title>
-  <?php
-    echo '<h1>Yeah, it works!<h1>';
-    // phpinfo();
-    $hostname = "db";
-    $username = "admin";
-    $password = "test";
-    $db = "database";
+<meta charset="iso-8559-1" />
+<meta name="description" content="Simulación de Linkedin" />
+<title> Vinculín </title>
 
-    $conn = mysqli_connect($hostname,$username,$password,$db);
-    if ($conn->connect_error) {
-      die("Database connection failed: " . $conn->connect_error);
+<body>
+  <header>
+    <h1>Vinculín</h1>
+  </header>
+  <nav>
+    <ul>
+      <li>Quiénes somos</li>
+      <li>Nuestros productos</li>
+      <li>Precios</li>
+    </ul>
+  </nav>
+
+  <aside>
+    <blockquote>Primer elemento</blockquote>
+    <blockquote>Segundo elemento</blockquote>
+    <blockquote>Tercero elemento</blockquote>
+  </aside>
+
+  <section>
+    <article>
+      <header>
+        <hgroup>
+          <h2> Titulo de la noticia 1 </h2>
+          <h3>Subtítulo de la noticia 1</h3>
+        </hgroup>
+      </header>
+
+      <p>Noticia 1</p>
+      <p>Seguimos hablando de noticia 1</p>
+      <p>Terminamos de hablar de la de noticia 1</p>
+
+      <footer>
+        <p> comentarios de usuarios </p>
+      </footer>
+
+    </article>
+
+    <article>
+      <header>
+        <h2>Título de la noticia 2</h2>
+
+      </header>
+      <p>Noticia 2</p>
+      <p>Seguimos hablando de noticia 2</p>
+      <p>Terminamos de hablar de la de noticia 2</p>
+    </article>
+
+    <article>
+      <header>
+        <hgroup>
+          <h2>Título de la noticia 3</h3>
+          <h3>Subtítulo de la noticia 3</h3>
+        </hgroup>
+
+      </header>
+      <p>Noticia 3</p>
+      <p>Seguimos hablando de noticia 3</p>
+      <p>Terminamos de hablar de la de noticia 3</p>
+    </article>
+
+  </section>
+
+  <footer>
+    Derechos reservados Tlfn: 123456789
+  </footer>
+
+
+  <?php
+      
+      // phpinfo();
+      $hostname = "db";
+      $username = "admin";
+      $password = "test";
+      $db = "database";
+
+      $conn = mysqli_connect($hostname,$username,$password,$db);
+      if ($conn->connect_error) {
+        die("Database connection failed: " . $conn->connect_error);
+      }
+
+
+
+    $query = mysqli_query($conn, "SELECT * FROM usuarios")
+      or die (mysqli_error($conn));
+
+    while ($row = mysqli_fetch_array($query)) {
+      echo
+      "<tr>
+        <td>{$row['id']}</td>
+        <td>{$row['nombre']}</td>
+      </tr>";
+      
+
     }
 
+    ?>
+</body>
 
-
-  $query = mysqli_query($conn, "SELECT * FROM usuarios")
-    or die (mysqli_error($conn));
-
-  while ($row = mysqli_fetch_array($query)) {
-    echo
-    "<tr>
-      <td>{$row['id']}</td>
-      <td>{$row['nombre']}</td>
-    </tr>";
-    
-
-  }
-
-  ?>
 </html>
