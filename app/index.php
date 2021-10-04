912c54b2fa105ba913c66d674a48345dce5bc0e1
DannyelYankee <daniel.juape@gmail.com>
2021-10-04 20:23:45 +0200

    Trasteando un poco con html

--- app/index.php
+++ app/index.php
@@ -1,29 +1,35 @@
-<?php
-  echo '<h1>Yeah, it works!<h1>';
-  // phpinfo();
-  $hostname = "db";
-  $username = "admin";
-  $password = "test";
-  $db = "database";
+<!DOCTYPE html>
+<html lang="es">
+  <meta charset="iso-8559-1"/>
+  <meta name="description" content="Simulación de Linkedin"/>
+  <title> Vinculín </title>
+  <?php
+    echo '<h1>Yeah, it works!<h1>';
+    // phpinfo();
+    $hostname = "db";
+    $username = "admin";
+    $password = "test";
+    $db = "database";
 
-  $conn = mysqli_connect($hostname,$username,$password,$db);
-  if ($conn->connect_error) {
-    die("Database connection failed: " . $conn->connect_error);
+    $conn = mysqli_connect($hostname,$username,$password,$db);
+    if ($conn->connect_error) {
+      die("Database connection failed: " . $conn->connect_error);
+    }
+
+
+
+  $query = mysqli_query($conn, "SELECT * FROM usuarios")
+    or die (mysqli_error($conn));
+
+  while ($row = mysqli_fetch_array($query)) {
+    echo
+    "<tr>
+      <td>{$row['id']}</td>
+      <td>{$row['nombre']}</td>
+    </tr>";
+    
+
   }
 
-
-
-$query = mysqli_query($conn, "SELECT * FROM usuarios")
-   or die (mysqli_error($conn));
-
-while ($row = mysqli_fetch_array($query)) {
-  echo
-   "<tr>
-    <td>{$row['id']}</td>
-    <td>{$row['nombre']}</td>
-   </tr>";
-   
-
-}
-
-?>
+  ?>
+</html>
