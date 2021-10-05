127f9bded3b3b6e844c41ae000b21f0e089e00b0
DannyelYankee <daniel.juape@gmail.com>
2021-10-05 09:12:38 +0200

    Vídeo 6

--- app/index.php
+++ app/index.php
@@ -3,80 +3,82 @@
 <meta charset="iso-8559-1" />
 <meta name="description" content="Simulación de Linkedin" />
 <title> Vinculín </title>
+<link rel="stylesheet" href="mihoja.css">
 
 <body>
-  <header>
-    <h1>Vinculín</h1>
-  </header>
-  <nav>
-    <ul>
-      <li>Quiénes somos</li>
-      <li>Nuestros productos</li>
-      <li>Precios</li>
-    </ul>
-  </nav>
+    <header id="cabeceraWeb">
+        <h1>Vinculín</h1>
+    </header>
+    <nav id="navegacionPrincipal">
 
-  <aside>
-    <blockquote>Primer elemento</blockquote>
-    <blockquote>Segundo elemento</blockquote>
-    <blockquote>Tercero elemento</blockquote>
-  </aside>
+        <ul>
+            <li>Quiénes somos</li>
+            <li>Nuestros productos</li>
+            <li>Precios</li>
+        </ul>
+    </nav>
 
-  <section>
-    <article>
-      <header>
-        <hgroup>
-          <h2> Titulo de la noticia 1 </h2>
-          <h3>Subtítulo de la noticia 1</h3>
-        </hgroup>
-      </header>
-      <figure>
-        <img src="imagenes/sol_mexico.jpeg" />
-        <figcaption>El mejor cantante del <strong>mundo</strong></figcaption>
+    <aside id="barraLateral">
+        <blockquote>Primer elemento</blockquote>
+        <blockquote>Segundo elemento</blockquote>
+        <blockquote>Tercero elemento</blockquote>
+    </aside>
 
-      </figure>
-      <p><mark>Noticia 1</mark></p>
-      <p>Seguimos hablando de noticia 1</p>
-      <p>Terminamos de hablar de la de noticia 1</p>
+    <section id="contenidoPrincipal">
+        <article>
+            <header>
+                <hgroup>
+                    <h2> Titulo de la noticia 1 </h2>
+                    <h3>Subtítulo de la noticia 1</h3>
+                </hgroup>
+            </header>
+            <figure>
+                <img src="imagenes/sol_mexico.jpeg" />
+                <figcaption>El mejor cantante del <strong>mundo</strong></figcaption>
 
-      <footer>
-        <p> comentarios de usuarios </p>
-      </footer>
+            </figure>
+            <p><mark>Noticia 1</mark></p>
+            <p>Seguimos hablando de noticia 1</p>
+            <p>Terminamos de hablar de la de noticia 1</p>
 
-    </article>
+            <footer>
+                <p> comentarios de usuarios </p>
+            </footer>
 
-    <article>
-      <header>
-        <h2>Título de la noticia 2</h2>
+        </article>
 
-      </header>
-      <p>Noticia 2</p>
-      <p>Vean <cite>Regreso al futuro</cite></p>
-      <p>Terminamos de hablar de la de noticia 2</p>
-      <time datetime="2021-10-04" pubdate> Noticia publicada el día 04/10/2021</time>
-    </article>
+        <article>
+            <header>
+                <h2>Título de la noticia 2</h2>
 
-    <article>
-      <header>
-        <hgroup>
-          <h2>Título de la noticia 3</h3>
-            <h3>Subtítulo de la noticia 3</h3>
-        </hgroup>
+            </header>
+            <p>Noticia 2</p>
+            <p>Vean <cite>Regreso al futuro</cite></p>
+            <p>Terminamos de hablar de la de noticia 2</p>
+            <time datetime="2021-10-04" pubdate> Noticia publicada el día 04/10/2021</time>
+        </article>
 
-      </header>
-      <p>Noticia 3</p>
-      <p>Seguimos hablando de noticia 3</p>
-      <p>Terminamos de hablar de la de noticia 3</p>
-    </article>
+        <article>
+            <header>
+                <hgroup>
+                    <h2>Título de la noticia 3</h3>
+                        <h3>Subtítulo de la noticia 3</h3>
+                </hgroup>
 
-  </section>
+            </header>
+            <p>Noticia 3</p>
+            <p>Seguimos hablando de noticia 3</p>
+            <p>Terminamos de hablar de la de noticia 3</p>
+        </article>
 
-  <footer>
-    <small> Derechos reservados <address>Tlfn 123456789</address> </small>
-  </footer>
+    </section>
+
+    <footer id="piePrincipal">
+        <small> Derechos reservados <address>Tlfn 123456789</address> </small>
+    </footer>
 
 
-  <?php
+    <?php
       
       // phpinfo();
       $hostname = "db";
