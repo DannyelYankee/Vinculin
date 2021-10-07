decd54739a2d9e5f39ce6dd49f44ac3920f967d7
DannyelYankee <daniel.juape@gmail.com>
2021-10-07 17:46:21 +0200

    cambios en index

--- app/html/index.php
+++ app/html/index.php
@@ -84,13 +84,13 @@
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-lg-6 py-3 wow fadeInUp">
-                    <span class="subhead">About us</span>
-                    <h2 class="title-section">Sign in to find your ideal job</h2>
+                    <span class="subhead">Acerca nuestra</span>
+                    <h2 class="title-section">Regístrate para encontrar el trabajo ideal para ti</h2>
                     <div class="divider"></div>
 
-                    <p>Find the more appropiate job or practice according to yo</p>
-                    <p>Publish a job advertisement so that millions of people can check it</p>
-                    <a href="about.html" class="btn btn-primary mt-3">Read More</a>
+                    <p>Encuentra el trabajo o las prácticas más apropiadas para ti.</p>
+                    <p>Publica una oferta de trabajo para que millones de personas puedan verla.</p>
+                    <a href="about.html" class="btn btn-primary mt-3">Saber más</a>
                 </div>
                 <div class="col-lg-6 py-3 wow fadeInRight">
                     <div class="img-fluid py-3 text-center">
@@ -101,230 +101,12 @@
         </div>
         <!-- .container -->
     </div>
-    <!-- .page-section -->
 
-    <div class="page-section bg-light">
-        <div class="container">
-            <div class="text-center wow fadeInUp">
-                <div class="subhead">Our services</div>
-                <h2 class="title-section">How SEO Team Can Help</h2>
-                <div class="divider mx-auto"></div>
-            </div>
 
-            <div class="row">
-                <div class="col-sm-6 col-lg-4 col-xl-3 py-3 wow zoomIn">
-                    <div class="features">
-                        <div class="header mb-3">
-                            <span class="mai-business"></span>
-                        </div>
-                        <h5>OnSite SEO</h5>
-                        <p>We analyse your website's structure, internal architecture & other key</p>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-lg-4 col-xl-3 py-3 wow zoomIn">
-                    <div class="features">
-                        <div class="header mb-3">
-                            <span class="mai-business"></span>
-                        </div>
-                        <h5>OnSite SEO</h5>
-                        <p>We analyse your website's structure, internal architecture & other key</p>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-lg-4 col-xl-3 py-3 wow zoomIn">
-                    <div class="features">
-                        <div class="header mb-3">
-                            <span class="mai-business"></span>
-                        </div>
-                        <h5>OnSite SEO</h5>
-                        <p>We analyse your website's structure, internal architecture & other key</p>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-lg-4 col-xl-3 py-3 wow zoomIn">
-                    <div class="features">
-                        <div class="header mb-3">
-                            <span class="mai-business"></span>
-                        </div>
-                        <h5>OnSite SEO</h5>
-                        <p>We analyse your website's structure, internal architecture & other key</p>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-lg-4 col-xl-3 py-3 wow zoomIn">
-                    <div class="features">
-                        <div class="header mb-3">
-                            <span class="mai-business"></span>
-                        </div>
-                        <h5>OnSite SEO</h5>
-                        <p>We analyse your website's structure, internal architecture & other key</p>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-lg-4 col-xl-3 py-3 wow zoomIn">
-                    <div class="features">
-                        <div class="header mb-3">
-                            <span class="mai-business"></span>
-                        </div>
-                        <h5>OnSite SEO</h5>
-                        <p>We analyse your website's structure, internal architecture & other key</p>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-lg-4 col-xl-3 py-3 wow zoomIn">
-                    <div class="features">
-                        <div class="header mb-3">
-                            <span class="mai-business"></span>
-                        </div>
-                        <h5>OnSite SEO</h5>
-                        <p>We analyse your website's structure, internal architecture & other key</p>
-                    </div>
-                </div>
-                <div class="col-sm-6 col-lg-4 col-xl-3 py-3 wow zoomIn">
-                    <div class="features">
-                        <div class="header mb-3">
-                            <span class="mai-business"></span>
-                        </div>
-                        <h5>OnSite SEO</h5>
-                        <p>We analyse your website's structure, internal architecture & other key</p>
-                    </div>
-                </div>
-            </div>
 
-        </div>
-        <!-- .container -->
-    </div>
-    <!-- .page-section -->
 
-    <div class="page-section banner-seo-check">
-        <div class="wrap bg-image" style="background-image: url(./assets/img/bg_pattern.svg);">
-            <div class="container text-center">
-                <div class="row justify-content-center wow fadeInUp">
-                    <div class="col-lg-8">
-                        <h2 class="mb-4">Check your Website SEO</h2>
-                        <form action="#">
-                            <input type="text" class="form-control" placeholder="E.g google.com">
-                            <button type="submit" class="btn btn-success">Check Now</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <!-- .container -->
-        </div>
-        <!-- .wrap -->
-    </div>
-    <!-- .page-section -->
 
-    <div class="page-section">
-        <div class="container">
-            <div class="text-center wow fadeInUp">
-                <div class="subhead">Pricing Plan</div>
-                <h2 class="title-section">Choose plan the right for you</h2>
-                <div class="divider mx-auto"></div>
-            </div>
-            <div class="row mt-5">
-                <div class="col-lg-4 py-3 wow zoomIn">
-                    <div class="card-pricing">
-                        <div class="header">
-                            <div class="pricing-type">Basic</div>
-                            <div class="price">
-                                <span class="dollar">$</span>
-                                <h1>39<span class="suffix">.99</span></h1>
-                            </div>
-                            <h5>Per Month</h5>
-                        </div>
-                        <div class="body">
-                            <p>25 Analytics <span class="suffix">Campaign</span></p>
-                            <p>1,300 Change <span class="suffix">Keywords</span></p>
-                            <p>Social Media <span class="suffix">Reviews</span></p>
-                            <p>1 Free <span class="suffix">Optimization</span></p>
-                            <p>24/7 <span class="suffix">Support</span></p>
-                        </div>
-                        <div class="footer">
-                            <a href="#" class="btn btn-pricing btn-block">Subscribe</a>
-                        </div>
-                    </div>
-                </div>
 
-                <div class="col-lg-4 py-3 wow zoomIn">
-                    <div class="card-pricing marked">
-                        <div class="header">
-                            <div class="pricing-type">Standar</div>
-                            <div class="price">
-                                <span class="dollar">$</span>
-                                <h1>59<span class="suffix">.99</span></h1>
-                            </div>
-                            <h5>Per Month</h5>
-                        </div>
-                        <div class="body">
-                            <p>25 Analytics <span class="suffix">Campaign</span></p>
-                            <p>1,300 Change <span class="suffix">Keywords</span></p>
-                            <p>Social Media <span class="suffix">Reviews</span></p>
-                            <p>1 Free <span class="suffix">Optimization</span></p>
-                            <p>24/7 <span class="suffix">Support</span></p>
-                        </div>
-                        <div class="footer">
-                            <a href="#" class="btn btn-pricing btn-block">Subscribe</a>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-4 py-3 wow zoomIn">
-                    <div class="card-pricing">
-                        <div class="header">
-                            <div class="pricing-type">Professional</div>
-                            <div class="price">
-                                <span class="dollar">$</span>
-                                <h1>99<span class="suffix">.99</span></h1>
-                            </div>
-                            <h5>Per Month</h5>
-                        </div>
-                        <div class="body">
-                            <p>25 Analytics <span class="suffix">Campaign</span></p>
-                            <p>1,300 Change <span class="suffix">Keywords</span></p>
-                            <p>Social Media <span class="suffix">Reviews</span></p>
-                            <p>1 Free <span class="suffix">Optimization</span></p>
-                            <p>24/7 <span class="suffix">Support</span></p>
-                        </div>
-                        <div class="footer">
-                            <a href="#" class="btn btn-pricing btn-block">Subscribe</a>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-        <!-- .container -->
-    </div>
-    <!-- .page-section -->
-
-    <!-- Banner info -->
-    <div class="page-section banner-info">
-        <div class="wrap bg-image" style="background-image: url(./assets/img/bg_pattern.svg);">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-6 py-3 pr-lg-5 wow fadeInUp">
-                        <h2 class="title-section">SEO to Improve Brand <br> Visibility</h2>
-                        <div class="divider"></div>
-                        <p>We're an experienced and talented team of passionate consultants who breathe with search engine marketing.</p>
-
-                        <ul class="theme-list theme-list-light text-white">
-                            <li>
-                                <div class="h5">SEO Content Strategy</div>
-                                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor</p>
-                            </li>
-                            <li>
-                                <div class="h5">B2B SEO</div>
-                                <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor</p>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="col-lg-6 py-3 wow fadeInRight">
-                        <div class="img-fluid text-center">
-                            <img src="./assets/img/banner_image_2.svg" alt="">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- .wrap -->
-    </div>
-    <!-- .page-section -->
 
     <!-- Blog -->
     <div class="page-section">
