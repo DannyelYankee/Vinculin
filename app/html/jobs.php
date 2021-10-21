db0caf40af30ab49cf7077d7110608e8db1300e0
soyAngela <angelagonzalez1999@hotmail.es>
2021-10-21 17:29:53 +0200

    index.php ahora es index.html

--- app/html/jobs.php
+++ app/html/jobs.php
@@ -28,7 +28,7 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-white sticky" data-offset="300">
             <div class="container">
-                <a href="index.php" class="navbar-brand">Vincul<span class="text-primary">in.</span></a>
+                <a href="index.html" class="navbar-brand">Vincul<span class="text-primary">in.</span></a>
 
                 <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -37,7 +37,7 @@
                 <div class="navbar-collapse collapse" id="navbarContent">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item">
-                            <a class="nav-link" href="index.php">Inicio</a>
+                            <a class="nav-link" href="index.html">Inicio</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="about.html">Acerca de</a>
@@ -161,8 +161,8 @@
                 <div class="col-lg-3 py-3">
                     <h5>Contáctanos</h5>
                     <p>Universidad del País Vasco, Ingeniero Torres Quevedo Plaza, 1, 48013 Bilbao, Vizcaya</p>
-                    <a href="#" class="footer-link">+34 123 456 78</a>
-                    <a href="#" class="footer-link">vinculin@inventedmail.com</a>
+                    <p>+34 123 456 78</p>
+                    <a href="mailto:vinculin@inventedmail.com" title="Enviar correo electronico">vinculin@inventedmail.com</a>
                 </div>
             </div>
 
