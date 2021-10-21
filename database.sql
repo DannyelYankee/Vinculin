bacb8808b6633d513209423b8eecf994040974c6
DannyelYankee <daniel.juape@gmail.com>
2021-10-21 17:04:52 +0200

    faltaba una linea al .sql

--- database.sql
+++ database.sql
@@ -26,6 +26,8 @@ SET time_zone = "+00:00";
 --
 -- Estructura de tabla para la tabla `Empleo`
 --
+CREATE DATABASE IF NOT EXISTS `database` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `database`;
 
 CREATE TABLE `Empleo` (
   `id` int(11) NOT NULL,
