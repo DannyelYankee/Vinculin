6fbfb498cf5846f4947c0f5ee70772147a0f8e4d
DannyelYankee <daniel.juape@gmail.com>
2021-12-16 17:54:16 +0100

    update-user-data con mysqli binding

--- app/html/update-user-data.php
+++ app/html/update-user-data.php
@@ -5,12 +5,12 @@ $hostname = "db";
 $username = "admin";
 $password = "test";
 $db = "database";
-$con = mysqli_connect($hostname,$username,$password,$db);
+$con = new mysqli($hostname,$username,$password,$db);
 if ($con->connect_error) {
     echo "Database connectin failed.";
     die("Database connection failed: " . $con->connect_error);
 }
-mysqli_select_db($con,$db);
+$con->select_db($db);
 $currentEmail = $_SESSION['usuario']['Email']; //email guardado en la sesion del usuario
 $NombreApellidos = $_POST["NombreApellidos"];
 $Email = $_POST["Email"];
@@ -21,18 +21,29 @@ $DNI= $_POST["dni"];
 $fNacimiento = $_POST["fNacimiento"];
 $Telefono = $_POST["Telefono"];
 
-$sql = mysqli_query($con,"UPDATE Usuario SET NombreApellidos='$NombreApellidos', Telefono='$Telefono', DNI='$DNI', FechaNacimiento='$fNacimiento' WHERE Email='$currentEmail'");
+//$sql = mysqli_query($con,"UPDATE Usuario SET NombreApellidos='$NombreApellidos', Telefono='$Telefono', DNI='$DNI', FechaNacimiento='$fNacimiento' WHERE Email='$currentEmail'");
+$sql= $con->prepare("UPDATE Usuario SET NombreApellidos=?, Telefono=?, DNI=?, FechaNacimiento=? WHERE Email=?");
+$sql->bind_param("sisss",$NombreApellidos,$Telefono,$DNI,$fNacimiento,$currentEmail);
+$sql->execute();
 if($currentEmail!=$Email){
-    
-    $updateEmailUsuario = "UPDATE Usuario SET Email='$Email' WHERE Email='$currentEmail'";
-    $updateEmailEmpleo = "UPDATE Empleo SET Email='$Email' WHERE Email='$currentEmail'";
-    $query_updateUsuario = mysqli_query($con,$updateEmailUsuario);
-    $query_updateEmpleo = mysqli_query($con,$updateEmailEmpleo);
+    //UPDATE Usuario SET Email='$Email' WHERE Email='$currentEmail'
+    $updateEmailUsuario =$con->prepare("UPDATE Usuario SET Email=? WHERE Email=?");
+    $updateEmailUsuario->bind_param("ss",$Email,$currentEmail);
+    $updateEmailUsuario->execute();
+    //UPDATE Empleo SET Email='$Email' WHERE Email='$currentEmail'
+    $updateEmailEmpleo = $con->prepare("UPDATE Empleo SET Email=? WHERE Email=?");
+    $updateEmailEmpleo->bind_param("ss",$Email,$currentEmail);
+    $updateEmailEmpleo->execute();
+    //$query_updateUsuario = mysqli_query($con,$updateEmailUsuario);
+    //$query_updateEmpleo = mysqli_query($con,$updateEmailEmpleo);
     $_SESSION['usuario']['Email'] = $Email;
 }
 
 if(md5("") != $Contraseña1 && $Contraseña2!=md5("")){
-    $sql2=mysqli_query($con,"UPDATE Usuario SET Contraseña='$Contraseña1' WHERE Email='$currentEmail'");
+    //"UPDATE Usuario SET Contraseña='$Contraseña1' WHERE Email='$currentEmail'"
+    $sql2=$con->prepare("UPDATE Usuario SET Contraseña=? WHERE Email=?");
+    $sql2->bind_param("ss",$Contraseña1,$currentEmail);
+    $sql2->execute();
     
 }
 
