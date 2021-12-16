dd9de54678c1000abdf0bc70aa3818c58503243e
DannyelYankee <daniel.juape@gmail.com>
2021-12-16 17:31:23 +0100

    register con binding mysqli

--- app/html/register.php
+++ app/html/register.php
@@ -15,29 +15,43 @@ $DNI = $_POST["dni"];
 $fNacimiento = $_POST["fNacimiento"];
 $Telefono = $_POST["Telefono"];
 
-$con = mysqli_connect($hostname, $username, $password, $db);
+$con = new mysqli($hostname, $username, $password, $db);
 if ($con->connect_error) {
     echo "Database connectin failed.";
     die("Database connection failed: " . $con->connect_error);
 }
-mysqli_select_db($con, $db);
+$con->select_db($db);
 
-$verificacion = mysqli_query($con, "SELECT * FROM Usuario where Email='$Email'");
+//$verificacion = mysqli_query($con, "SELECT * FROM Usuario where Email='$Email'");
+$stmt = $con->prepare("SELECT * FROM Usuario where Email=?");
+$stmt->bind_param("s", $Email);
+$stmt->execute();
+$verificacion = $stmt->get_result();
 echo mysqli_num_rows($verificacion);
-if (mysqli_num_rows($verificacion) > 0) {
+$row = $verificacion->fetch_assoc();
+if ($row) {
     echo "<script> alert('Ya existe un usuario con ese email.'); </script>";
     echo '<script> window.location.replace("./signup.php"); </script>';
     exit();
 }
-$sql = "insert into Usuario (NombreApellidos, Email, Contraseña, DNI, FechaNacimiento, Telefono) values ('$NombreApellidos','$Email',md5('$Contraseña'),'$DNI','$fNacimiento','$Telefono');";
-mysqli_query($con, $sql);
-$_SESSION['usuario']['Email'] = $Email;    
+//'$NombreApellidos','$Email',md5('$Contraseña'),'$DNI','$fNacimiento','$Telefono'
+$sql = $con->prepare("insert into Usuario (NombreApellidos, Email, Contraseña, DNI, FechaNacimiento, Telefono) values (?,?,?,?,?,?)");
+$sql->bind_param("sssssi", $NombreApellidos, $Email, md5($Contraseña), $DNI, $fNacimiento, $Telefono);
+$sql->execute();
+$result = $sql->get_result();
+
+
+
+
+$_SESSION['usuario']['Email'] = $Email;
 $_SESSION['usuario']['NombreApellidos'] = $NombreApellidos;
-$_SESSION['usuario']['DNI']=$row['DNI'];
-$_SESSION['usuario']['FechaNacimiento']=$fNacimiento;
-$_SESSION['usuario']['Telefono']=$Telefono;
-$_SESSION['usuario']['Contraseña']=$Contraseña;
+$_SESSION['usuario']['DNI'] = $row['DNI'];
+$_SESSION['usuario']['FechaNacimiento'] = $fNacimiento;
+$_SESSION['usuario']['Telefono'] = $Telefono;
+$_SESSION['usuario']['Contraseña'] = $Contraseña;
 header("Location: ./index.php");
+
+
 
 
 mysqli_close($con)
