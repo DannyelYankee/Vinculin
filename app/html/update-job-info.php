f3000a8171e5cc455f4b0d51e96579bb52750fc9
DannyelYankee <daniel.juape@gmail.com>
2021-12-16 17:41:36 +0100

    update-job-info con mysqli binding

--- app/html/update-job-info.php
+++ app/html/update-job-info.php
@@ -6,20 +6,22 @@ $username = "admin";
 $password = "test";
 $db = "database";
 
-$con = mysqli_connect($hostname,$username,$password,$db);
+$con = new mysqli($hostname,$username,$password,$db);
 if ($con->connect_error) {
     echo "Database connectin failed.";
     die("Database connection failed: " . $con->connect_error);
 }
-mysqli_select_db($con,$db);
+$con->select_db($db);
 
 $id = $_GET['id'];
 $titulo = $_POST["titulo"];
 $empresa = $_POST["empresa"];
 $descripcion = $_POST["descripcion"];
 $localidad = $_POST["localidad"];
-
-mysqli_query($con,"UPDATE Empleo SET Titulo='$titulo', Empresa='$empresa', Descripcion='$descripcion', Localidad ='$localidad' WHERE id ='$id'");
+//"UPDATE Empleo SET Titulo='$titulo', Empresa='$empresa', Descripcion='$descripcion', Localidad ='$localidad' WHERE id ='$id'"
+$update= $con->prepare("UPDATE Empleo SET Titulo= ?, Empresa= ?, Descripcion=?, Localidad=? WHERE id=?");
+$update->bind_param("ssssi",$titulo,$empresa,$descripcion,$localidad,$id);
+$update->execute();
 header("Location: datos-usuario.php");
 
 mysqli_close($con);
