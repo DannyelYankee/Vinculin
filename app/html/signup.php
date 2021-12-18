75c889725eb704ca5e230c0791e4de485404c2e4
DannyelYankee <daniel.juape@gmail.com>
2021-12-18 19:00:07 +0100

    boton de ver contraseña en login, tampoco funciona xd

--- app/html/signup.php
+++ app/html/signup.php
@@ -82,14 +82,14 @@
                 return;
             }
             //comprobar formato de la contraseña (?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"
-            /*
+            
             var expresion_regular_contra = /"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"/;
             if (!expresion_regular_contra.test(password)) {
                 alert('Contraseña incorrecta. Tu contraseña debe tener mínimo ocho caracteres, al menos una letra mayúscula, una letra minúscula, un número y un carácter especial.');
                 window.location.replace("signup.php");
                 return;
             }
-            */
+            
             //comprobar que el dni cumple con el formato
 
             var numero;
