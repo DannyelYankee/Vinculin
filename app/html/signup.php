9bc72ae8298d4abcdb7b2d079a87236f181a7c74
DannyelYankee <daniel.juape@gmail.com>
2021-12-19 22:35:45 +0100

    contraseña exprs regular arregaldo

--- app/html/signup.php
+++ app/html/signup.php
@@ -58,21 +58,22 @@
             }
 
 
+            //comprobar formato de la contraseña (?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"
+            
+            var expresion_regular_contra = /(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}/;
+            if (!expresion_regular_contra.test(password)) {
+                alert('Contraseña incorrecta. Tu contraseña debe tener mínimo ocho caracteres, al menos una letra mayúscula, una letra minúscula, un número y un carácter especial.');
+                window.location.replace("signup.php");
+                return;
+                
+            }
+            
             //comprobar que contraseña y repite contraseña coinciden
             if (password != password2) {
                 alert("Las contraseñas no coinciden. ");
                 window.location.replace("signup.php");
                 return;
             }
-            //comprobar formato de la contraseña (?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"
-            
-            var expresion_regular_contra = /"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"/;
-            if (!expresion_regular_contra.test(password)) {
-                alert('Contraseña incorrecta. Tu contraseña debe tener mínimo ocho caracteres, al menos una letra mayúscula, una letra minúscula, un número y un carácter especial.');
-                window.location.replace("signup.php");
-                return;
-            }
-            
             //comprobar que el dni cumple con el formato
 
             var numero;
